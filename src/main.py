2724fdb516d76d3add55f9d93ab5f4af3f82ccb4
itsharshitrwt <harshit.rawat1804@gmail.com>
2025-02-06 00:16:55 +0530

    new feature

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 
 def test():
     assert add(1, 2) == 3
     assert add(2, 3) == 5
+    assert subtract(3, 2) == 1
+    assert subtract(2, 3) == -1
+
     
